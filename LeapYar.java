f4b8249ba0afba0f0235dd7c59f055a70d71605f
jbaek1 <89271872+jbaek1@users.noreply.github.com>
2021-11-03 13:36:09 -0500

    11/3

---
+++ LeapYar.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class LeapYar here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LeapYar
+{
+     public static void isLeapYear() {
+    {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a year!");
+        int year = scan.nextInt();
+        if (year % 400 ==0
+    }
+}
+}
+
