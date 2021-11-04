53d1a9c8dfcbd34730dbb20b29b55aa690443dd5
jbaek1 <89271872+jbaek1@users.noreply.github.com>
2021-11-04 13:13:50 -0500

    11/4

--- Iterations.java
+++ Iterations.java
@@ -4,10 +4,30 @@ import java.util.Scanner;
 /**
  * Examples and notes on iterations (Chapter 6)
  *
- * @author gcschmit
+ * @author jbaek
  * @version 16nov2020
  */
 public class Iterations
 {
-    
+    public static void whileExample()
+    {
+        /* 
+         * while loop:
+         * evaluates a boolean expression
+         * if true, executes the body of the loop and then re-evaluates the condition
+         * if false, skips the body and continues
+         * 
+         */
+        int count =1; // initialization
+        while (count <=5)//condition
+        {
+            System.out.println(count); // body
+            count++; //update the looping variable
+            
+        }
+        System.out.println("Done.");
+        // careful for infinite loops; often a result of not properly updating the loop variable or 
+        // writing an impossible loop condition.
+        
+    }
 }
