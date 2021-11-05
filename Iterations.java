89e363daf1d6518d9f1bd4ca18ce9e7bcc831c5c
jbaek1 <89271872+jbaek1@users.noreply.github.com>
2021-11-05 12:51:49 -0500

    11/5

--- Iterations.java
+++ Iterations.java
@@ -30,4 +30,45 @@ public class Iterations
         // writing an impossible loop condition.
         
     }
-}
+    public static void forExample()
+    {
+        /*
+         * for loop:
+         * Three parts of the for loop statement
+         * 1. initialization - executed once
+         * 2. looping condition - boolean expression evaluated at the start of each iteration
+         * 3. update the loop variable - executed at the *end* of each iteation *before* evaluating the condition again
+         * 
+         */
+        for (int count = 1; // initialization
+        count <=5; // condition
+        count ++)
+        {
+            System.out.println(count); // body
+            
+        }
+            System.out.println("done");
+            /*
+             * variables declared within the for statement are scoped to the for statement and its body
+             * 
+             */
+        }
+        public static void offByOne()
+        {
+            /*
+             * the infamous off-by-one error is common with for loops
+             * executing one too many or one too few times
+             * should initial value start at 0 or 1?
+             * should the condition < or <=?
+             * 
+             */
+            for (int i = 0;
+            i<=5;
+            i++)
+            {
+                System.out.println("*");
+                
+            }
+        }
+    }
+
